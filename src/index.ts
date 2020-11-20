a687532cd23f1e793c250928e7cb9eb51b614037
mhmdanas <32234660+mhmdanas@users.noreply.github.com>
2020-11-20 16:29:00 +0300

    Make the bot start doing its job

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,42 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application } from "probot"; // eslint-disable-line no-unused-vars
+import { IssuesGetResponseData } from "@octokit/types";
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on("issues.labeled", async ({ octokit, ...context }) => {
+    const issue = context.issue();
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    async function closeIssue() {
+      await octokit.issues.update(context.issue({ state: "closed" }));
+    }
+
+    async function getAuthor(): Promise<IssuesGetResponseData["user"]> {
+      return (await octokit.issues.get(issue)).data.user;
+    }
+
+    const issueLabels = (await octokit.issues.listLabelsOnIssue(issue)).data;
+
+    const issueLabelNames = issueLabels.map((label) => label.name);
+
+    if (issueLabelNames.includes("doesn't-follow-template")) {
+      await closeIssue();
+
+      const author = await getAuthor();
+
+      const issueComment = context.issue({
+        body: `Hi @${author.login}! Thank you for opening an issue! Unfortunately, it looks like you haven't followed the template or read its instructions carefully. Please read the template again and do what it says; it will ensure both you and team members have a far easier time discussing the issue!`,
+      });
+
+      await octokit.issues.createComment(issueComment);
+    } else if (issueLabelNames.includes("template-ignored")) {
+      await closeIssue();
+
+      const author = await getAuthor();
+
+      const issueComment = context.issue({
+        body: `Hi @${author.login}. Thank you for opening an issue! Please edit your issue and follow the template provided here [TODO] and a team member will get in touch with you.`,
+      });
+
+      await octokit.issues.createComment(issueComment);
+    }
+  });
+};
